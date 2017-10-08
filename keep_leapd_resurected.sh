7d8b36e5f5d9e92126690356cb3e1b05d5942530
Zvonimir Fras <zvonimir@zvonimirfras.com>
2017-10-08 13:48:18 -0400

    Give the opportunity to kill script

--- keep_leapd_resurected.sh
+++ keep_leapd_resurected.sh
@@ -3,4 +3,5 @@
 # leapd keeps segfaulting so bring it back up every time it dies, phoenix style
 while true; do
     leapd
+    sleep 0.5  # give the opportunity to kill the loop and exit the script
 done
